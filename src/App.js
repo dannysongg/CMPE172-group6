99ab74d6f102d45558b9c276a26afb7c249789fc
dannysongg <dannysongg@gmail.com>
2021-05-09 16:27:59 -0700

    Yongwang's contribution: backend and database

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-//import logo from './logo.svg';
-import './App.css';
+//test comment
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Movie from './Movies';
-import Seats from './Seats';
-import Navigation from './Navigation';
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-
+import Movie from './views/Movies'
+import Seats from './views/Seats'
+import Navigation from './Navigation'
+import Login from './views/Login'
+//change//
+import './App.css'
 class App extends Component {
-  render() {
-    return (      
-       <BrowserRouter>
+  render () {
+    return (
+      <BrowserRouter>
         <div>
           <Navigation />
-            <Switch>
-             <Route path="/" component={Movie} exact/>
-             <Route path ="/seats" component={Seats} exact/>
-           </Switch>
-        </div> 
+          <Switch>
+            <Route path='/dashboard' component={Movie} exact />
+            <Route path='/seats' component={Seats} exact />
+            <Route path='/' component={Login} exact />
+          </Switch>
+        </div>
       </BrowserRouter>
-    );
+    )
   }
 }
- 
-export default App;
+
+export default App
